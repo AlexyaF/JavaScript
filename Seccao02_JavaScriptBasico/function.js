c2a202846bd0b360ae3b116b939289aba27e032e
AlexyaF <alexya.fortunato@hotmail.com>
2024-11-14 10:09:54 -0300

    aula de funções

---
+++ Seccao02_JavaScriptBasico/function.js
@@ -0,0 +1,36 @@
+// funcao sem retorno
+function saudacao(nome) {
+    console.log(`Bom dia! ${nome}`);
+    return nome;
+}
+
+saudacao("ALEXYA");
+const variavel = saudacao("Alexya");
+console.log(variavel);
+
+
+
+// funcao com retorno
+function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log(soma(6,8));
+const result = soma(2,4);
+console.log(result);
+
+console.log('alexya', 'fortunato');
+
+
+// No caso de adcionar uma funcao a uma variavel
+const raiz = function (n) {
+    return n **0.5;
+
+}; // necessário ponto e virgula nesse caso
+
+console.log(raiz(5));
+
+// Arrow function
+const raiz2 = n => n**5;
+console.log(raiz2(6));
