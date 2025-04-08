7ca9f949bd1b0d09335034c57fbf0cf2d10052ea
Alexya Fortunato <allexya.fortunato@hotmail.com>
2025-04-08 17:56:45 -0300

    Acressentando mais explicações

--- Seccao03/operadoresLogicos.js
+++ Seccao03/operadoresLogicos.js
@@ -3,6 +3,9 @@
 // ! = NOT
 
 console.log(true && true); // todas expressães precisam ser verdadeiras para retornar true
-console.log(true || false); // apenas uma precisa ser verdadeira para retornar true
+console.log(true || false); // apenas uma precisa ser verdadeira para retornar true - sempre retorna o primeiro valor verdadeiro
 
 console.log(!true); // inverte o valor/nega o valor
+
+// valores que são conaiderados FALSY (não é o falso literal mas rtorna FALSE):
+// 0, '' "" ``, null, undefined, NaN
