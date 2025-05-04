84ba80a1ad8a24ed7de168548128f27fb5e94194
Alexya Fortunato <alexya.fortunato@crefaz.com.br>
2025-05-04 20:05:31 -0300

    Executando exercicio sozinha

---
+++ ex03/js/script.js
@@ -0,0 +1,28 @@
+const elementos = [
+    {tag: 'p', texto: 'Frase 1'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'footer', texto: 'Frase 3'},
+    {tag: 'section', texto: 'Frase 4'}
+
+]
+
+function findElement(){
+    const result = document.querySelector('.resultado');
+    return result
+}
+
+function addNaTela(){
+    const resultado = findElement();
+    for (let i = 0; i < elementos.length; i++){
+        const {tag, texto: frase} = elementos[i]; //Nomear a variavel o valor vem antes 'valor --> variavel'
+        console.log(tag, frase);
+        const element = document.createElement(tag);
+        element.textContent = frase;
+        
+
+    resultado.appendChild(element);
+    }
+}
+
+
+addNaTela();
