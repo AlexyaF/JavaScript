e4da3dd6e342499e74c1e73578f0f5f505d3370c
Alexya Fortunato <allexya.fortunato@hotmail.com>
2025-06-03 16:06:54 -0300

    novo execicio

--- Seccao03/logicaProgramacao.js
+++ Seccao03/logicaProgramacao.js
@@ -26,3 +26,31 @@ console.log(ePaisagem(1080, 1920));
 // arrow function
 const ePaisagemArrowF = (width, heigh) => width > heigh;
 console.log(ePaisagemArrowF(1080, 1920));
+
+//==============================================================================================
+
+//  Funçao que recebe um numero e classifica da seguinte forma:
+//  Se for divisível por 3 = Fizz
+//  Se for divisível por 5 = Buzz
+//  Dívisivel por ambos = FizzBuzz
+//  Não é divisível por nenhum = retorna o próprio número
+//   - Checar se realmente é um número = retornar próprio número
+//   - Função com números de 0 a 100
+
+function fizzBuzz(n){
+    if (Number.isNaN(n)){
+        return n
+    }
+    
+    if (n % 3 === 0 && n % 5 === 0){
+        return 'FizzBuzz'
+    }else if (n % 5 === 0) {
+        return 'Buzz'
+    }else if (n % 3 === 0){
+        return 'Fizz'
+    }else{
+        return n
+    }
+}
+
+console.log(fizzBuzz('a'));
