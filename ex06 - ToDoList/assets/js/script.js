d9b1bf6ccbec5a0caae58e74844fdaed83a10f3f
Alexya Fortunato <allexya.fortunato@hotmail.com>
2025-06-10 16:29:30 -0300

    função responsável por adicionar as tarefas a lista

--- ex06 - ToDoList/assets/js/script.js
+++ ex06 - ToDoList/assets/js/script.js
@@ -1,9 +1,15 @@
 const InputTask = document.querySelector('.input_new_task');
 const addTaskButon = document.querySelector('.btn__add__task');
-const tasks = document.querySelector('.tasks');
+const tasks = document.querySelector('.task__list');
+
+function creatNewTask(text) {
+    const li = document.createElement('li');
+    li.innerText = text;
+    tasks.appendChild(li);
+}
 
 addTaskButon.addEventListener('click', function() {
     if (!InputTask.value) return;
 
-    console.log(InputTask.value);
+    creatNewTask(InputTask.value);
 });
