60a002eadc19329a186ba9b99f2e8340748cb031
Alexya Fortunato <allexya.fortunato@hotmail.com>
2025-06-17 16:43:19 -0300

    Criando todas as funções de C and D

--- ex06 - ToDoList/assets/js/script.js
+++ ex06 - ToDoList/assets/js/script.js
@@ -6,13 +6,56 @@ function creatNewTask(text) {
     const li = document.createElement('li');
     li.innerText = text;
     tasks.appendChild(li);
-    clearInput()
+    clearInput();
+    addButonDelete(li);
+    saveTask();
 }
 
 function clearInput() {
     InputTask.value = '';
     InputTask.focus();
 }
+
+function addButonDelete(li){
+    li.innerText += " ";
+    const deleteButon = document.createElement('button');
+    deleteButon.innerText = 'Delete';
+    li.appendChild(deleteButon);
+    deleteButon.setAttribute('class', 'btn__delete__task');
+}
+
+
+function saveTask(){
+    const liTasks = tasks.querySelectorAll('li');
+    const listTasks = [];
+
+    for (let task of liTasks){
+        let taskText = (task.innerText);
+        taskText = taskText.replace('Delete', '').trim();
+        listTasks.push(taskText);
+    };
+    const jsonTasks = JSON.stringify(listTasks);
+    localStorage.setItem('Tasks', jsonTasks);
+}
+
+function addSaveTasks(){
+    const saveTasks = localStorage.getItem('Tasks');
+    const taskList = JSON.parse(saveTasks);
+    for (let tasks of taskList){
+        creatNewTask(tasks);
+    }
+}
+
+addSaveTasks();
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+
+    if (el.classList.contains('btn__delete__task')) {
+        el.parentElement.remove();
+        saveTask();
+    };
+})
 
 InputTask.addEventListener('keypress', function(e){
     if (e.keyCode === 13) {
