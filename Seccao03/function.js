67de320b6fcb3f02464186bdccfd8dfa10f23859
Alexya Fortunato <allexya.fortunato@hotmail.com>
2025-06-17 16:57:04 -0300

    arrow function e função dentro de um objeto

--- Seccao03/function.js
+++ Seccao03/function.js
@@ -1,10 +1,24 @@
+// First-class objects (Objetos de primeira classe) - pode ser usada como dado
 // Declaração de função (Function hoisting) - eleva a função ao incicio do código
 function falaOi(){
     console.log('oi');
 }
 
-// First-class objects (Objetos de primeira classe)
 // Function expression - criar função e atribuir á uma variável
 const souUmDado = function(){
     console.log('oi');
-}
+};
+
+
+//Arrow function
+const funcaoArrow = () => {
+    console.log('oi');
+};
+
+//Dentro de um objeto
+const obj = {
+    falar: function(){
+        console.log('oi');
+    }
+};
+obj.falar();
